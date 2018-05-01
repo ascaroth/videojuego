15742d2fd6b433fdb8d6401e089ea4525f8ff180
ascaroth <clistarasta@hotmail.com>
2018-04-30 19:50:26 -0500

    Add files via upload

---
+++ PlayerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed = 5.0f;
+	public float padding = 1.0f;
+
+	private float xmax = -5f;
+	private float xmin = 5f;
+
+	// Use this for initialization
+	void Start () {
+		Camera camera = Camera.main;
+		float distance = transform.position.z - camera.transform.position.z;
+		xmin = camera.ViewportToWorldPoint(new Vector3(0,0, distance)).x + padding;
+		xmax = camera.ViewportToWorldPoint(new Vector3(1,1, distance)).x - padding;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position = new Vector3 (Mathf.Clamp(transform.position.x -speed* 
+				Time.deltaTime, xmin, xmax), transform.position.y, transform.position.z);
+		}
+
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.position = new Vector3 (Mathf.Clamp(transform.position.x +speed*  
+				Time.deltaTime, xmin, xmax), transform.position.y, transform.position.z);
+		}
+	}
+}
---
+++ generadordeenemigos.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generadordeenemigos : MonoBehaviour {
+
+	public GameObject enemyprefab;
+	public float ancho = 10;
+	public float alto = 5;
+	public float velocidad = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in transform) {
+			GameObject enemigo = Instantiate (enemyprefab, child.position , Quaternion.identity) as GameObject;
+		enemigo.transform.parent = child;
+		}
+		
+	}
+
+	void OnDrawGizmos (){
+		float xmin = transform.position.x - 0.5f * ancho;
+		float xmax = transform.position.x + 0.5f * ancho;
+		float ymin = transform.position.x - 0.5f * alto;
+		float ymax = transform.position.x + 0.5f * alto;
+		Gizmos.DrawLine (new Vector3 (xmin, ymin, 0), new Vector3(xmin,ymax, 0));
+		Gizmos.DrawLine (new Vector3 (xmin, ymax, 0), new Vector3(xmax,ymax, 0));
+		Gizmos.DrawLine (new Vector3 (xmax, ymax, 0), new Vector3(xmax,ymin, 0));
+		Gizmos.DrawLine (new Vector3 (xmax, ymin, 0), new Vector3(xmin,ymin, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
